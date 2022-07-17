d6b9bc7d1d9def05a5dadee55b87daf4d8623a81
Harsh Agarwal <harshagarwal13072000@gmail.com>
2022-07-17 14:31:19 +0530

    Flutter

---
+++ speechtext/lib/main.dart
@@ -0,0 +1,208 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Voice',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SpeechScreen(),
+    );
+  }
+}
+
+class SpeechScreen extends StatefulWidget {
+  const SpeechScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SpeechScreen> createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  final Map<String, HighlightedWord> _highlights = {
+    'flutter': HighlightedWord(
+        onTap: () => print("Flutter"),
+        textStyle: const TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ))
+  };
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = "Press the button and start speaking";
+  double _confidence = 1.0;
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        endRadius: 75.0,
+        glowColor: Theme.of(context).primaryColor,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: TextHighlight(
+            text: _text,
+            words: _highlights,
+            textStyle: const TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+}
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   stt.SpeechToText _speechToText = stt.SpeechToText();
+//   bool _speechEnabled = false;
+//   String _lastWords = '';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initSpeech();
+//   }
+
+//   /// This has to happen only once per app
+//   void _initSpeech() async {
+//     _speechEnabled = await _speechToText.initialize();
+//     setState(() {});
+//   }
+
+//   /// Each time to start a speech recognition session
+//   void _startListening() async {
+//     await _speechToText.listen(onResult: _onSpeechResult);
+//     setState(() {});
+//   }
+
+//   /// Manually stop the active speech recognition session
+//   /// Note that there are also timeouts that each platform enforces
+//   /// and the SpeechToText plugin supports setting timeouts on the
+//   /// listen method.
+//   void _stopListening() async {
+//     await _speechToText.stop();
+//     setState(() {});
+//   }
+
+//   /// This is the callback that the SpeechToText plugin calls when
+//   /// the platform returns recognized words.
+//   void _onSpeechResult(SpeechRecognitionResult result) {
+//     setState(() {
+//       _lastWords = result.recognizedWords;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Speech Demo'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Container(
+//               padding: EdgeInsets.all(16),
+//               child: Text(
+//                 'Recognized words:',
+//                 style: TextStyle(fontSize: 20.0),
+//               ),
+//             ),
+//             Expanded(
+//               child: Container(
+//                 padding: EdgeInsets.all(16),
+//                 child: Text(
+//                   // If listening is active show the recognized words
+//                   _speechToText.isListening
+//                       ? '$_lastWords'
+//                       // If listening isn't active but could be tell the user
+//                       // how to start it, otherwise indicate that speech
+//                       // recognition is not yet ready or not supported on
+//                       // the target device
+//                       : _speechEnabled
+//                           ? 'Tap the microphone to start listening...'
+//                           : 'Speech not available',
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed:
+//             // If not yet listening for speech start, otherwise stop
+//             _speechToText.isNotListening ? _startListening : _stopListening,
+//         tooltip: 'Listen',
+//         child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+//       ),
+//     );
+//   }
+// }
